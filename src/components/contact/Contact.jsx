25fd7e3d318ce9ffe27add618675bc492c75f192
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-01 14:35:20 +0200

    unpdate contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,21 +11,22 @@ export const Contact = () => {
           <ul className={css.list}>
               <li className={css.listItem}>
                   <img src={icon1} alt="phone" className={css.icon} />
-                  <p className={css.text}>+357 9056759</p>
+                  <a href='+3579056759' className={css.text}>+357 9056759</a>
               </li>
               <li className={css.listItem}>
                   <img src={icon2} alt="message" className={css.icon} />
-                  <p className={css.text}>contact@pamrelocation.com</p>
+                  <a href='contact@pamrelocation.com' className={css.text}>contact@pamrelocation.com</a>
               </li>
           </ul>
 
           <div className={css.card}>
-              <h4>send inquiry</h4>
+              <h4 className={css.cardTitle}>send inquiry</h4>
               <Formik
             initialValues={{
               name: '',
               email: '',
               phone: '',
+              subject: '',
               message: '',
               terms: false
             }}
@@ -33,9 +34,11 @@ export const Contact = () => {
               name: Yup.string().required('Name is required'),
               email: Yup.string().email('Invalid email').required('Email is required'),
               phone: Yup.string().required('Phone is required'),
+              subject: Yup.string().required('Subject is required'),
               message: Yup.string().required('Message is required'),
               terms: Yup.boolean().oneOf([true], 'You must accept terms and conditions')
             })}
+                  
             onSubmit={(values, { setSubmitting }) => {
               setTimeout(() => {
                 alert(JSON.stringify(values, null, 2));
@@ -44,22 +47,25 @@ export const Contact = () => {
             }}
           >
             {({ isSubmitting }) => (
-                      <Form>
+                      <Form className={css.form}>
                 <Field type="text" name="name" placeholder="Name" className={css.input} />
-                <ErrorMessage name="name" component="div" />
+                <ErrorMessage name="name" component="div" className={css.error} />
     
                 <Field type="email" name="email" placeholder="Email" className={css.input} />
-                <ErrorMessage name="email" component="div" />
+                <ErrorMessage name="email" component="div" className={css.error} />
     
                 <Field type="text" name="phone" placeholder="Phone" className={css.input} />
-                <ErrorMessage name="phone" component="div" />
+                <ErrorMessage name="phone" component="div" className={css.error} />
+                          
+                 <Field type="text" name="subject" placeholder="Subject" className={css.input} />
+                <ErrorMessage name="subject" component="div" className={css.error} />
     
                 <Field as="textarea" name="message" placeholder="Message" className={css.textarea} />
-                <ErrorMessage name="message" component="div" />
+                <ErrorMessage name="message" component="div" className={css.error} />
     
                 <Field type="checkbox" name="terms" className={css.checkbox}  />
                 <label htmlFor="terms" className={css.label}>By clicking “Submit button” you accept our Terms & Conditions and have read our Privacy Policy and Disclaimer</label>
-                <ErrorMessage name="terms" component="div" />
+                <ErrorMessage name="terms" component="div" className={css.error} />
     
                 <button type="submit" disabled={isSubmitting} className={css.btn}>
                   {isSubmitting ? 'Submitting' : 'Submit'}
